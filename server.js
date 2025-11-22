6c860fca861a2df58503b73f3b559d10d7cdd821
Ashutosh Shukla <ashuushukla47@gmail.com>
2025-11-22 15:49:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public")); // static files
+
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
